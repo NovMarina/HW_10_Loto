0df8b2926d076cb8e27778d270e512a36fbb6952
NovMarina <marina.novikova1998@gmail.com>
2025-05-29 00:43:29 +0300

    third commit (+even,odd & generation unique numbers)

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ const max = 36, min = 1, arraysLength = 5;
 const defInfo = document.getElementById("def")
 const rsltInfo = document.getElementById("result");
 const boxRslt = document.querySelector(".box-result");
+const bigBoxGenNumb = document.querySelector(".box-numbers");
+const genNumbersBox = document.querySelectorAll(".box-numbers>div");
 
 function generateNumbers() {
+    let newNum;
     let numbers = new Array(arraysLength);
-    for (let i = 0; i < numbers.length; i++) {
-        numbers[i] = Math.floor(Math.random() * (max - min + 1) + min);
+    for (let i = 0; i < numbers.length;) {
+        newNum = Math.floor(Math.random() * (max - min + 1) + min);
+        if (!numbers.includes(newNum)) {
+            numbers[i++] = newNum
+        }
     }
     return numbers;
 }
 
 function sortGenNumsAsc(genNums) {
-    return genNums.sort((a, b) => a - b);
+    return genNums.sort((a, b) => b - a);
 }
 
 function calcAvrg(genNums) {
@@ -24,44 +30,76 @@ function calcAvrg(genNums) {
     return (sum / genNums.length).toFixed(2);
 }
 
-function calcMaxNumber(genNums) {
+function calcMinNumber(genNums) {
     return genNums[genNums.length - 1];
 }
 
-function calcMinNumber(genNums) {
+function calcMaxNumber(genNums) {
     return genNums[0];
 }
 
-function statistics(minNumber, maxNumber, avg) {
+function statistics(minNumber, maxNumber, avg, even, odd) {
     const info = {
         "Min value": minNumber,
         "Max value": maxNumber,
         "Average": avg,
+        "Number of even": even,
+        "Number of odd": odd,
         toString: function () {
             return JSON.stringify(info);
         }
     };
+
+    //-------№1------
     rsltInfo.innerHTML =
         `<p>Min value: ${minNumber}</p>
 <p>Max value: ${maxNumber}</p>
-<p>Average : ${avg}</p>`;
+<p>Average : ${avg}</p>
+<p>Number of even: ${even}</p>
+<p>Number of odd: ${odd}</p>`;
+
+//--------№2--------
+//     let htmlText = '<table>';
+//     for (const [key, value] of Object.entries(info)) {
+//         if (key !== 'toString')
+//             htmlText += `<tr><td>${key}:</td><td>${value}</td></tr>`;
+//     }
+//     htmlText += '</table>';
+//     rsltInfo.innerHTML = htmlText;
 }
 
+
+function inGenBox(genNums) {
+    for (let i = 0; i < genNums.length; i++)
+        genNumbersBox[i].textContent = genNums[i];
+}
+
+function calcEvenOdd(genNums, flag) {//true=even, false=odd
+    let countEven = 0;
+    let countOdd = 0;
+    genNums.forEach(genNum => {
+        if (genNum % 2 === 0) countEven++;
+        else countOdd++;
+    })
+    if (flag === true)
+        return countEven;
+    else return countOdd;
+}
 
 buttonClick.onclick = function () {
     let genNums;
     genNums = generateNumbers();
     genNums = sortGenNumsAsc(genNums);
-    const minNumber = calcMinNumber(genNums);
-    const maxNumber = calcMaxNumber(genNums);
-    const avg = calcAvrg(genNums);
-    statistics(minNumber, maxNumber, avg);
+    statistics(calcMinNumber(genNums), calcMaxNumber(genNums), calcAvrg(genNums), calcEvenOdd(genNums, true), calcEvenOdd(genNums, false));
+    bigBoxGenNumb.classList.remove("d-none");
+    inGenBox(genNums);
     defInfo.style.display = "none";
     rsltInfo.style.display = "block";
     boxRslt.style.alignContent = "center";
 }
 
 boxRslt.ondblclick = function () {
+    bigBoxGenNumb.classList.add("d-none");
     defInfo.style.display = "block";
     rsltInfo.style.display = "none";
 }
